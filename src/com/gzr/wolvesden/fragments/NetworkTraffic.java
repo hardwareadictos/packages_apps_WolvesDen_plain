AICP/packages_apps_AicpExtras

Add expanded status bar network traffic setting
Change-Id: I940d93f50f5d7f22ff932f31654a5605f7f58c2b

src/com/aicp/extras/fragments/NetworkTraffic.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2018 The Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.aicp.extras.fragments;
+
+import com.aicp.extras.BaseSettingsFragment;
+import com.aicp.extras.R;
+
+public class NetworkTraffic extends BaseSettingsFragment {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.status_bar_network_traffic;
+    }
+}

hardwareadictos/packages_apps_WolvesDen_plain

src/com/gzr/wolvesden/fragments/NetworkTraffic.java
@@ -1,0 +1,28 @@
+/*
+ * Copyright (C) 2018 The Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.gzr.wolvesden.fragments;
+
+import com.android.settings.R;
+import com.gzr.wolvesden.BaseSettingsFragment;
+
+public class NetworkTraffic extends BaseSettingsFragment {
+
+    @Override
+    protected int getPreferenceResource() {
+        return R.xml.status_bar_network_traffic;
+    }
+}